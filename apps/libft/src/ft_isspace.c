fe19de39a2b1baa67aa31318c7791f93171546de
Linux - Thuis <rutgermeuzelaar@gmail.com>
2024-08-05 18:04:23 +0200

    edited ft_isspace

--- apps/libft/src/ft_isspace.c
+++ apps/libft/src/ft_isspace.c
@@ -6,11 +6,11 @@
 /*   By: rmeuzela <rmeuzela@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/08/05 15:23:44 by rmeuzela      #+#    #+#                 */
-/*   Updated: 2024/08/05 15:24:17 by rmeuzela      ########   odam.nl         */
+/*   Updated: 2024/08/05 16:40:34 by rmeuzela      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isspace(char c)
+int	ft_isspace(int c)
 {
 	return (c == '\f' || c == '\n' || c == '\r' || c == '\t' || c == '\v');
 }
